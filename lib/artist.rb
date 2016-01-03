19a28041d0563db4dd454d45e465b769af27bc14
Chris Bruen <bruen.chris@gmail.com>
2016-01-03 13:19:31 -0500

    finish

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,16 @@
 require 'pry'
 
 require_relative '../lib/memorable.rb'
+require_relative '../lib/paramable.rb'
 
 class Artist
+  extend Memorable
+  include Paramable
+  
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
-
-#  def self.find_by_name(name)
-#    @@artists.detect{|a| a.name}
-#  end
 
   def initialize
     @@artists << self
@@ -21,14 +21,6 @@ class Artist
     @@artists
   end
 
-  #def self.reset_all
-  #  self.all.clear
-  #end
-
-  #def self.count
-  #  @@artists.count
-  #end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -38,8 +30,5 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 
 end
