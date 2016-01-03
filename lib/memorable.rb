7932d88bcbd17f25ce243a0be6ea68fca100bb16
Chris Bruen <bruen.chris@gmail.com>
2016-01-03 13:21:03 -0500

    done

--- lib/memorable.rb
+++ lib/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
-	def self.find_by_name(name)
+	def find_by_name(name)
 		self.all.detect {|x| x.name == name}
 	end
-	def self.count
+	def count
 		self.all.count
 	end
-	def self.reset_all
+	def reset_all
 		self.all.clear
 	end
 end
