a62431047402e195098bfd2c2734afb99cd96add
coltonstaab1 <colton.staab@gmail.com>
2016-01-11 21:11:55 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,18 @@
 require 'pry'
+require_relative 'memorable.rb'
 
 class Artist
+  extend Memorable
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
-
+=begin
   def self.find_by_name(name)
     @@artists.detect{|a| a.name}
   end
-
+=end
   def initialize
     @@artists << self
     @songs = []
@@ -18,7 +21,7 @@ class Artist
   def self.all
     @@artists
   end
-
+=begin
   def self.reset_all
     self.all.clear
   end
@@ -26,7 +29,7 @@ class Artist
   def self.count
     @@artists.count
   end
-
+=end
   def add_song(song)
     @songs << song
     song.artist = self
