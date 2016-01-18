d9b903e6d18ae7799a07ca27ffa00b53ea53988b
codeesh <vineshpersaud@gmail.com>
2016-01-18 13:06:08 -0500

    Done.

---
+++ lib/memorable.rb
@@ -0,0 +1,21 @@
+
+module Memorable
+ 
+ def find_by_name(name)
+    self.all.detect{|a| a.name}
+ end
+
+
+
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+   def to_param
+    name.downcase.gsub(' ', '-')
+  end
+
+end
