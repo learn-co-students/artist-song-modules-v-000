26f74a6be4cbf256a0bd916017ded35b06182e54
shmulim <shmuel.markel@gmail.com>
2016-01-19 14:15:28 -0800

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,17 @@
+module Memorable
+
+  @@all = []
+
+  def reset_all
+    @@all.clear
+  end
+
+  def count
+    @@all.count
+  end
+
+  def find_by_name(name)
+    @@all.detect{|a| a.name}
+  end
+
+end
