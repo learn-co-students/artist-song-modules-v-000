29900f4e6cd9e66a9228e98e8beff51d56103aa6
benser1 <briganenser28@gmail.com>
2016-01-21 15:34:37 -0700

    Done.

---
+++ lib/memorable.rb
@@ -0,0 +1,20 @@
+
+
+module Memorable
+
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+
+  def find_by_name(name)
+    self.all.detect do |a|
+      a.name 
+    end
+  end
+
+
+end
