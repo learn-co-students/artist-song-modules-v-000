44a5e3650e10fcca1b1441e887d8166636fe6951
krisnotchris <krishenderson@gmail.com>
2016-02-01 20:30:09 -0600

    Done.

---
+++ lib/memorable.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+module Memorable
+
+  def reset_all
+    self.all.clear
+  end
+
+  def find_by_name(name)
+    self.all.detect {|a| a.name}
+  end
+
+  def count
+    self.all.count
+  end
+
+end
