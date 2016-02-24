9b4144c08206d3612323a53a26d1fc09ee963387
abadfish <kirsten.ofarrell@gmail.com>
2016-02-24 16:03:40 -0500

    Done

---
+++ lib/memorable.rb
@@ -0,0 +1,17 @@
+module Memorable
+
+  attr_accessor :name
+
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+
+  def find_by_name(name)
+    self.all.detect {|a| a.name}
+  end
+
+end
