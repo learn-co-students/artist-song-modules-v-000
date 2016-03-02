4ac2763ee74d5918bb201129105d381fde165cef
cernanb <cernanb@gmail.com>
2016-03-02 13:24:01 -0800

    refactored code to add modules for identical code in Artist and Song classes

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,17 @@
 require 'pry'
+require_relative '../lib/memorable.rb'
+require_relative '../lib/paramable.rb'
+
+
 
 class Artist
   attr_accessor :name
   attr_reader :songs
 
-  @@artists = []
+  extend Memorable
+  include Paramable
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name}
-  end
+  @@artists = []
 
   def initialize
     @@artists << self
@@ -17,14 +20,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
   end
 
   def add_song(song)
@@ -36,8 +31,8 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
 end
