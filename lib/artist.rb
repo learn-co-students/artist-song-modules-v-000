c848c39e676131ac49eec09e60c0502b21ae228c
kaileeagray <kaileeagray@gmail.com>
2016-05-18 10:20:08 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,9 +6,9 @@ class Artist
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name}
+  # end
 
   def initialize
     @@artists << self
@@ -19,13 +19,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  #
+  # def self.count
+  #   @@artists.count
+  # end
 
   def add_song(song)
     @songs << song
---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,11 @@
+# def self.find_by_name(name)
+#   @@artists.detect{|a| a.name}
+# end
+
+# def self.reset_all
+#   self.all.clear
+# end
+#
+# def self.count
+#   @@artists.count
+# end
