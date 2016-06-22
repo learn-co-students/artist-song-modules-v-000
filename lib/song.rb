fd9c156e00eacbd69004eaa748fc5c90fb72e1cd
Michael Stewart <upp_mike@yahoo.com>
2016-06-22 22:30:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,35 +1,25 @@
+require_relative '../config/environment.rb'
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Findable
+  include Paramable
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name}
-  end
-
   def artist=(artist)
     @artist = artist
   end
-
-  def self.reset_all
-    @@songs.clear
-  end
-
+=begin
   def to_param
     name.downcase.gsub(' ', '-')
   end
-
-  def self.count
-    self.all.count
-  end
-
+=end
   def self.all
     @@songs
   end
