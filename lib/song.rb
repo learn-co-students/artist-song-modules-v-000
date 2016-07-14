91f4c7fb55dbe92d92bda3d0d7d211a09d9bcff9
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-14 03:28:59 +0000

    Done

--- lib/song.rb
+++ lib/song.rb
@@ -1,38 +1,24 @@
-# require 'pry'
+require 'pry'
 
 class Song
-  extend Memorable::ClassMethods
+
+  extend Memorable::ClassMethods #inherit the class method included in Memorable.rb
+  extend Findable::ClassMethods #inherit the class method included in Findable.rb
+  include Paramable::InstanceMethods #inherit the class method included in Paramable.rb
+  include Memorable::InstanceMethods #inherit the class method included in Memorable.rb
 
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
   def artist=(artist)
     @artist = artist
   end
 
-  # def self.reset_all
-  #   @@songs.clear
-  # end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  # def self.count
-  #   self.all.count
-  # end
-
   def self.all
     @@songs
   end
+
 end
