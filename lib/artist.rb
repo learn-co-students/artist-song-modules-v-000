51ffdac7f25b7da804ddfeb5aa50ccec4e55f363
tylertaylor <vikingtyty@gmail.com>
2016-07-30 12:00:06 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,9 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
 
   @@artists = []
 
@@ -11,20 +14,12 @@ class Artist
   end
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
   end
 
   def add_song(song)
@@ -34,10 +29,6 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 
 end
