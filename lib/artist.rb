d91b4be73cb9c6824a870fbe1a4a14f3e1d16395
Clare <clare.e.horton@gmail.com>
2016-09-08 02:02:52 +0000

    refactored #to_param

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,12 +6,9 @@ class Artist
   
   extend Memorable
   extend Findable
+  include Paramable
 
   @@artists = []
-
- #  def self.find_by_name(name)
- #   @@artists.detect{|a| a.name == name}
- #  end
 
   def initialize
     @@artists << self
@@ -29,10 +26,6 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 
 end
