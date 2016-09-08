cb3278a284d216645c3b2b3d81fddc7d79a1e258
Clare <clare.e.horton@gmail.com>
2016-09-08 01:46:33 +0000

    refactored #reset_all and #count methods for Artist and Song

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,8 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
+  
+  extend Memorable
 
   @@artists = []
 
@@ -17,14 +19,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
   end
 
   def add_song(song)
