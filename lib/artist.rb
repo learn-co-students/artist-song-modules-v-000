dbbcecc0b9a042961908aadfba9be7c225eb9611
Mary Umoh <umohm12@gmail.com>
2016-11-22 17:54:42 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,9 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  include Paramable
+  include Paramable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
