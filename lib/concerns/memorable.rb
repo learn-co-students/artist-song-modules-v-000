65126324539bc01e57ef75f9e2301401acd8e64d
mxdavis <md01@me.com>
2017-03-05 02:10:37 +0200

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,11 @@
 module Memorable
-  def self.reset_all
-    self.all.clear
-  end
 
-  def self.count
-    self.all.count
-  end
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+
 end
