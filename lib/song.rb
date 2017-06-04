8c0104b74f61970977d56ae4ba7b4be108cf4633
Jacilyn Hayden <jacihayden@gmail.com>
2017-06-04 02:23:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -12,17 +12,9 @@ class Song
   # @@songs = [] ** could also use @@songs, but i tried @@all - not sure which is the best?
   @@all = []
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
-
   def artist=(artist)
     @artist = artist
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 
   def self.all
     @@all
