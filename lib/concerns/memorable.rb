b1e9b25145d7b0df225bf215aae5d324013ae9d8
ctab33 <ctab33@me.com>
2017-06-05 18:09:39 -0400

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,18 +1,25 @@
 module Memorable
 
-  def self.reset_all
-    self.all.clear
+  def reset_all
+    all.clear
   end
 
-  def self.count
-    @@artists.count
+  def count
+    all.count
   end
-
-  def self.count
-    self.all.count
-  end
-
-  def self.reset_all
-    @@songs.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  #
+  # def self.count
+  #   @@artists.count
+  # end
+  #
+  # def self.count
+  #   self.all.count
+  # end
+  #
+  # def self.reset_all
+  #   @@songs.clear
+  # end
 end
