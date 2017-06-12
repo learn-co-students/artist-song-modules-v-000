7b3d89ed5657a7bafadc46ea2068b63e56516685
Alex <github email address>
2017-06-12 18:56:21 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,28 +3,26 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+#  def self.find_by_name(name)
+#    @@artists.detect{|a| a.name == name}
+#  end
 
   def initialize
-    @@artists << self
+    #Remember our super keyword from the inheritance code along exercise? The super keyword, placed inside a method, will tell that method to look up its behavior in the method of the same name that lives in the parent, or super, class. A method that includes the super keyword will execute any code placed inside the super class' method of the same name, and then execute any code inside the child class' method.
+    #So, we can use the super keyword to tell our Artist's .initialize method to use the code in the Memorable::InstanceMethods module's .initialize method and also to use any additional code in the Artist's .initialize method. Take a look:
+    #When we include a module in a class, we are really telling that class to inherit methods from that module.
+   super
     @songs = []
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
   end
 
   def add_song(song)
@@ -36,8 +34,8 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+#  def to_param
+#    name.downcase.gsub(' ', '-')
+#  end
 
 end
---
+++ lib/concerns/paramable.rb
@@ -0,0 +1,8 @@
+#for instance module
+module Paramable
+  module InstanceMethods
+  def to_param
+      name.downcase.gsub(' ', '-')
+  end
+end
+end
