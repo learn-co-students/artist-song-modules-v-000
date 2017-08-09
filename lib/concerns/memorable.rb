3d40df060176e21e98e781e41ca67c8c83ba12d0
Dominique De Leon <dominique.a.deleon@gmail.com>
2017-08-09 16:49:02 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,19 +1,27 @@
 module Memorable
   module ClassMethods
-    def self.count #artist class method
-      @@artists.count
-    end
+    # def count #artist class method
+    #   @@artists.count
+    # end
+    #
+    # def count #song class method
+    #   self.all.count
+    # end
 
-    def self.count #song class method
+    def count
       self.all.count
     end
 
-    def self.reset_all #artist class method
-      self.all.clear
-    end
+    # def reset_all #artist class method
+    #   self.all.clear
+    # end
+    #
+    # def reset_all #song class method
+    #   @@songs.clear
+    # end
 
-    def self.reset_all #song class method
-      @@songs.clear
+    def reset_all
+      self.all.clear
     end
 
   end
