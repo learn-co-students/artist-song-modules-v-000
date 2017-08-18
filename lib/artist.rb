38be690b66cc9118ea2219c4032bf22083cc603c
Dominique McCloud <niquelejon@gmail.com>
2017-08-18 19:25:19 +0000

    completed

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,16 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+
 
   def initialize
     @@artists << self
@@ -36,8 +38,6 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+
 
 end
