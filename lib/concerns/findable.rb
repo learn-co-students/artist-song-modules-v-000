2b526b7ee9a6364aeb2cc02fb4ac8fb84d333f79
Gui Bin Zhang <guibzhang@gmail.com>
2017-09-19 01:14:04 +0000

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,9 @@
+module Findable
+  module ClassMethods
+
+    def find_by_name(name)
+      self.all.detect{|s| s.name == name}
+    end
+  end
+
+end
---
+++ lib/concerns/paramable.rb
@@ -0,0 +1,13 @@
+# def to_param
+#   name.downcase.gsub(' ', '-')
+# end
+
+module Paramable
+  module InstanceMethods
+
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
+  end
+
+end
