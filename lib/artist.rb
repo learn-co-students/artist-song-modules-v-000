825e91c16a63a89e232b2dc12ea9e7e777d9c5e2
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-28 15:51:48 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,23 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  extend Findable
+  include Memorable::InstanceMethods
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
   end
 
   def self.count
@@ -34,10 +31,6 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 
 end
