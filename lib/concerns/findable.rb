a02df190acdd16a3ed7b696dcaf32196c4807063
Christopher DiStasio <github email address>
2017-11-16 12:43:13 -0600

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,15 +1,14 @@
 module Findable
     module InstanceMethods
 
-        def find_by_name(name)
-            all.detect{|a| a.name == name}
-        end
-
+   
     end
 
     module ClassMethods
     
-        
+        def find_by_name(name)
+            all.detect{|a| a.name == name}
+        end
     end
 end
      
