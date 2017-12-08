35d4fceba586f80b29de0937faf1b0cedec31c25
charityyoungblood <youngblood.charity@gmail.com>
2017-12-08 14:16:35 -0800

    created file paramable.rb; added placeholder for module Paramable in paramable.rb

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Findable 
  
   def find_by_name(name) # class method - goes through the artists array and returns the name of the artist
-    self.all.detect{|a| a.name == name}
+    self.all.detect {|a| a.name == name}
   end
   
 end 
