8c1a99191162242d471245c42668fc617b91cb16
charityyoungblood <youngblood.charity@gmail.com>
2017-12-08 13:58:00 -0800

    deleted class variables, replaced with self

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,18 +1,12 @@
 module Memorable
+  
     def reset_all # class method - sets the songs array to empty
-    @@songs.clear
+    self.all.clear
   end
 
     def count # class method - calls all and count method on Song class
     self.all.count
   end
 
-   def reset_all # class method - sets the artists array to an empty array - clears array
-    self.all.clear
-  end
 
-  def count # class method - counts the indexes of the artists array (.length)
-    @@artists.count
-  end
-  
 end 
