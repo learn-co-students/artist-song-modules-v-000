69d99cda77dab3cb1647a90f54522273a48218c5
charityyoungblood <youngblood.charity@gmail.com>
2017-12-08 12:32:57 -0800

    added comments for what is occuring in code for song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,24 @@
 require 'pry'
 
 class Song
-  attr_accessor :name
-  attr_reader :artist
+  attr_accessor :name # access to call name method - manipulate value
+  attr_reader :artist # reader only - cannot manipulate value for artist
 
-  @@songs = []
+  @@songs = [] # class variable set to empty array
 
-  def initialize
-    @@songs << self
+  def initialize # initializes a new instance of Song class - w/o argument
+    @@songs << self # upon initialization Song instances are stored in songs array
   end
 
-  def self.find_by_name(name)
+  def self.find_by_name(name) # class method - iterates over the songs array - returns song name attr
     @@songs.detect{|a| a.name == name}
   end
 
-  def artist=(artist)
+  def artist=(artist) # instance setter method - sets the artist instance variable to value that is passed in as argument 
     @artist = artist
   end
 
-  def self.reset_all
+  def self.reset_all # class method - 
     @@songs.clear
   end
 
