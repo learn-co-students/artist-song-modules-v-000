b5a8a44fab3ed3fc864f069280ec83e5f17ef851
charityyoungblood <youngblood.charity@gmail.com>
2017-12-08 13:37:35 -0800

    added reset_all and count method for Song class to memorable.rb

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,12 @@
 module Memorable
-  
+    def self.reset_all # class method - sets the songs array to empty
+    @@songs.clear
+  end
+
+    def self.count # class method - calls all and count method on Song class
+    self.all.count
+  end
+
    def self.reset_all # class method - sets the artists array to an empty array - clears array
     self.all.clear
   end
