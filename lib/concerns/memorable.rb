6220aec254b4887d32d108f77089408dcfab020b
charityyoungblood <youngblood.charity@gmail.com>
2017-12-08 13:20:17 -0800

    added reset_all and clear methods to memorable.rb

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,3 +1,11 @@
 module Memorable
   
+   def self.reset_all # class method - sets the artists array to an empty array - clears array
+    self.all.clear
+  end
+
+  def self.count # class method - counts the indexes of the artists array (.length)
+    @@artists.count
+  end
+  
 end 
