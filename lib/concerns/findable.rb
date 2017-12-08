a6710aa362b11f3c09eefa0b32da018587103505
charityyoungblood <youngblood.charity@gmail.com>
2017-12-08 14:07:06 -0800

    deleted self keyword and class variable - replaced with self.all method to access class variable

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,7 @@
 module Findable 
  
-  def self.find_by_name(name) # class method - goes through the artists array and returns the name of the artist
-    @@artists.detect{|a| a.name == name}
+  def find_by_name(name) # class method - goes through the artists array and returns the name of the artist
+    self.all.detect{|a| a.name == name}
   end
   
-    def self.find_by_name(name) # class method - iterates over the songs array - returns song name attr
-    @@songs.detect{|a| a.name == name}
-  end
 end 
