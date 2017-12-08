bcb729047d00c17656c88c3d2e9d75379f9d12c6
charityyoungblood <youngblood.charity@gmail.com>
2017-12-08 14:03:33 -0800

    added find_by_name method to module Findable

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,4 +1,7 @@
 module Findable 
-  
+ 
+  def self.find_by_name(name) # class method - goes through the artists array and returns the name of the artist
+    @@artists.detect{|a| a.name == name}
+  end
   
 end 
