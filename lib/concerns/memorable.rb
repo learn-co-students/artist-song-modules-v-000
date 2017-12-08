803879ce2cb60a0c4d11eb5b8de6d34382aa3d93
charityyoungblood <youngblood.charity@gmail.com>
2017-12-08 13:40:33 -0800

    deleted self keyword from reset_all and count method in memorable.rb

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,17 +1,17 @@
 module Memorable
-    def self.reset_all # class method - sets the songs array to empty
+    def reset_all # class method - sets the songs array to empty
     @@songs.clear
   end
 
-    def self.count # class method - calls all and count method on Song class
+    def count # class method - calls all and count method on Song class
     self.all.count
   end
 
-   def self.reset_all # class method - sets the artists array to an empty array - clears array
+   def reset_all # class method - sets the artists array to an empty array - clears array
     self.all.clear
   end
 
-  def self.count # class method - counts the indexes of the artists array (.length)
+  def count # class method - counts the indexes of the artists array (.length)
     @@artists.count
   end
   
