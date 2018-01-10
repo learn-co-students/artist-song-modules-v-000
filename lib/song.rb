a78c66a6e3d5fc9e3b9e58301d2f2dfe329fcce1
Brad Hambrice <hambrice@uab.edu>
2018-01-10 08:26:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,34 +1,39 @@
 require 'pry'
+require_relative "./concerns/memorable.rb"
+require_relative "./concerns/findable.rb"
+require_relative "./concerns/paramable.rb"
 
 class Song
   attr_accessor :name
   attr_reader :artist
-
+extend Memorable
+extend Findable
+include Paramable
   @@songs = []
 
   def initialize
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+ # def self.find_by_name(name)
+#    @@songs.detect{|a| a.name == name}
+#  end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
+ # def self.reset_all
+ #   @@songs.clear
+  #end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+ # def to_param
+ #   name.downcase.gsub(' ', '-')
+ # end
 
-  def self.count
-    self.all.count
-  end
+  #def self.count
+  #  self.all.count
+ # end
 
   def self.all
     @@songs
