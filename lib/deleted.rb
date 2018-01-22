8fd2a9b34857ae8eb1234c4f86e1a63da160eb4b
K Logic <kellyayers411@gmail.com>
2018-01-22 00:41:44 +0000

    Done.

--- lib/deleted.rb
+++ lib/deleted.rb
@@ -34,4 +34,7 @@
     @@songs.detect{|a| a.name == name}
   end
   
+  =begin
+Instead of requiring individual files within one another, as you may have noticed we did in the previous two code along exercises, we created an environment file to handle those requirements for us.
+=end
   =end
