8c426c550e0cdc1df91bcd84575138ff0a354f5e
Jayon Kim <jpkim921@gmail.com>
2018-03-04 22:33:13 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,43 +1,39 @@
 require 'pry'
 
 class Artist
+
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
+  # where all artist class will be held
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
+  # initializes adding itself into the class variable and creating an array to hold the has many songs
   def initialize
-    @@artists << self
+    # @@artists << self
+    super
     @songs = []
   end
 
+  # reader method to read @@artists
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
-  end
-
+  # adds song to has many songs array and also creates belongs to association to song's artist
   def add_song(song)
     @songs << song
     song.artist = self
   end
 
+  # takes an array of songs and adds each and all songs to the artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 
 end
