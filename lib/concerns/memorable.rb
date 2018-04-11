a01c7e404ac390cd2ae5e0fd0d1b56d08da6012d
Hiroki Kato <hkato@sas.upenn.edu>
2018-04-11 16:51:56 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,18 @@
-module Memorable
 
-  def self.reset_all
-    self.all.clear
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
   end
 
-  def self.count
-    self.all.count
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
 end
