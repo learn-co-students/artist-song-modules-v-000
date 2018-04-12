39cc615094057bed6751fa9b0f97418101983338
EC2 Default User <ec2-user@ip-172-31-9-0.ec2.internal>
2018-04-12 06:13:19 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,21 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
+  
+  
+  
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
+  
+  def initialize
+    super
+      @songs = []
+  end  
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
@@ -19,14 +30,6 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
-  end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -35,9 +38,10 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
+  
+=begin
   def to_param
     name.downcase.gsub(' ', '-')
   end
-
+=end
 end
