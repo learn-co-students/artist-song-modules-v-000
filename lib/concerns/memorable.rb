7143b7fea624dec74a3ef728164100cdab99b9b7
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-04-20 13:30:12 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,13 +1,23 @@
 module Memorable
   
-  @@artists = []
+  module ClassMethods
   
-  def reset_all
-    @@artists.clear
+    def reset_all
+      self.all.clear
+    end
+    
+    def count
+      self.all.count
+    end
+  
   end
+    
+  module InstanceMethods
+    
+    def initialize
+      self.class.all << self
+    end
   
-  def count
-    @@artists.count
   end
   
 end
