749818677076256375e813f24245e1fb42edd1dc
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-17 18:10:19 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
+  extend Memorable
 
   @@artists = []
 
@@ -19,14 +20,6 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
-  end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -39,5 +32,4 @@ class Artist
   def to_param
     name.downcase.gsub(' ', '-')
   end
-
 end
