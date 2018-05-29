ed60e84fc73ffb3143f1eaf99ea6ccf0af80e542
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-29 21:22:46 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,11 @@
-# require 'pry'
-
 class Artist
-  extend Memorable 
+  extend Memorable, Findable
+  include Paramable
   
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
 
   def initialize
     @@artists << self
@@ -21,14 +16,6 @@ class Artist
     @@artists
   end
 
-  #def self.reset_all
-    #self.all.clear
-  #end
-
-  #def self.count
-    #@@artists.count
-  #end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -37,9 +24,4 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,6 @@
-# require 'pry'
-
 class Song
-  extend Memorable 
+  extend Memorable, Findable
+  include Paramable
   
   attr_accessor :name
   attr_reader :artist
@@ -12,25 +11,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def artist=(artist)
     @artist = artist
   end
-
-  #def self.reset_all
-    #@@songs.clear
-  #end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  #def self.count
-    #self.all.count
-  #end
 
   def self.all
     @@songs
