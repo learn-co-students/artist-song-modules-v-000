81e9cb052b78e3ea9076fb1201d00bd9905b1e8e
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-29 05:04:44 +0000

    Done.

---
+++ lib/concerns/ClassMethods.rb
@@ -0,0 +1,10 @@
+  
+module ClassMethods 
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
 require 'pry'
+require_relative '../config/environment.rb'
+require_relative './concerns/ClassMethods.rb'
 
 class Song
+  extend ClassMethods 
+  
   attr_accessor :name
   attr_reader :artist
 
@@ -18,17 +22,17 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
+  #def self.reset_all
+    #@@songs.clear
+  #end
 
   def to_param
     name.downcase.gsub(' ', '-')
   end
 
-  def self.count
-    self.all.count
-  end
+  #def self.count
+    #self.all.count
+  #end
 
   def self.all
     @@songs
