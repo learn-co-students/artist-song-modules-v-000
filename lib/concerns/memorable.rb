09e7200d9513668918a9d5bf155601f5f8df7e46
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-31 21:44:53 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,18 @@
 
 module Memorable
-
-  def count
-    self.all.count
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
 
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    def count
+      self.all.count
+    end
 
+    def reset_all
+      self.all.clear
+    end
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -4,15 +4,12 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
 
   def artist=(artist)
     @artist = artist
