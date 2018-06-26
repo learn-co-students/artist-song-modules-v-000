abcbe9a489b26d2973f6156bf6b4e86ee071eb3c
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-26 03:16:29 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,16 @@
 module Memorable
+  module ClassMethods
   def reset_all
     self.all.clear
   end
 
  def count
-   class.count
+   self.all.count
   end
+end
+module InstanceMethods
+  def initialize
+    self.class.all << self
+  end
+end
 end
