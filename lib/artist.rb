0a518c2918c43ff2312663d12c1d8c2560395816
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-01 19:29:08 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,6 +5,7 @@ class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
@@ -15,7 +16,7 @@ class Artist
   #end
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -8,4 +8,11 @@ module Memorable
       all.count
     end
   end
+
+  module InstanceMethods
+
+    def initialize
+      self.class.all <<  self
+    end
+  end
 end
