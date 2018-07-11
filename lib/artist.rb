2ef980752fd74d504a072cbb24f70e39e4ec8969
Jamie Brown <jmbrowncow@gmail.com>
2018-07-11 14:56:05 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,16 +2,17 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods, Findable
-  include Paramable Memorable::InstanceMethods
+  include Paramable, Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  # def initialize
-  #   @songs = []
-  # end
+  def initialize
+    super
+    @songs = []
+  end
 
   def self.all
     @@artists
--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,12 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods, Findable
-  include Paramable Memorable::InstanceMethods
+  include Paramable, Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    super
-    @@songs << self
-  end
 
   def artist=(artist)
     @artist = artist
