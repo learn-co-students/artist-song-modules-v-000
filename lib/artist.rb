e0448bcecf9709ae859f1f49a22238caeeb5c037
Angie Kassab <anasab12@gmail.com>
2018-08-29 20:08:59 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,6 +19,44 @@ class Artist
     @@artists
   end
 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
+
+end
+
+=begin
+The code below is correct without the concerns folder and modules:
+require 'pry'
+
+class Artist
+  attr_accessor :name
+  attr_reader :songs
+
+  @@artists = []
+
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
+
+  def initialize
+    @@artists << self
+    @songs = []
+  end
+
+  def self.all
+    @@artists
+  end
+
   def self.reset_all
     self.all.clear
   end
@@ -41,3 +79,5 @@ class Artist
   end
 
 end
+
+=end
