a07d734642675c63d575168a2a98efe6a742f596
Harley Rayner <harleymteden@gmail.com>
2018-09-13 17:22:22 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,7 @@ class Artist
   attr_reader :songs
 
   extend Memorable
+  extend Findable
   include Paramable
 
   @@artists = []
--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,7 @@ class Song
   attr_reader :artist
 
   extend Memorable
+  extend Findable
   include Paramable
 
   @@songs = []
