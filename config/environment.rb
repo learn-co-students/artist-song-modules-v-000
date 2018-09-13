8cd1ba070e42724612a3ab03ea35c2a19c347302
Steven Crouse <sdcrouse91@gmail.com>
2018-09-13 06:23:01 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
+require_relative '../lib/concerns/nameable' # This file was not required for the Codealong.
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
---
+++ lib/concerns/nameable.rb
@@ -0,0 +1,3 @@
+module Nameable # Note: this is not required; I simply wanted to see if it could be done.
+  attr_accessor :name
+end
