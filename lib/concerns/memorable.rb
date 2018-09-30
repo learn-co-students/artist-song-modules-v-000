1cc5ea3736d79bf8e908f3c1c740a93983b65ca4
John Werenski <jtwerenski@gmail.com>
2018-09-30 18:50:15 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,27 @@
+module Memorable
+  
+  module ClassMethods
+    
+    def reset_all
+      self.all.clear
+    end 
+    
+    def count
+      self.all.count 
+    end
+    
+  end
+  
+  module InstanceMethods
+    
+   # def to_param
+    #  self.name.downcase.gsub(' ', '-')
+  #  end
+    
+    def initialize 
+      self.class.all << self 
+    end 
+    
+  end 
+  
+end 
