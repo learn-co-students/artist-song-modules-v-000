1d666d9e382c4716b724c08ff0324bcb5bf70bdd
Ashley Wong <ashwong04@gmail.com>
2018-10-02 01:38:47 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,11 +2,17 @@ module Memorable
   
   module ClassMethods
     def reset_all
-      self.all.clear
+      self.class.all.clear
     end
 
     def count
-      self.all.count
+      self.class.all.count
+    end
+  end
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
     end
   end
   
