83afb75f0aa85b36706870e5f90d604d63a0f77c
Achsah Abraham <achsahmp@gmail.com>
2018-10-16 22:11:24 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,61 @@
 require 'pry'
+require_relative './memorable.rb'
+require_relative './paramable.rb'
+#class Artist
+#  attr_accessor :name
+#  attr_reader :songs
+#  extend Memorable::ClassMethods
+#  extend Findable
+#  include Memorable::InstanceMethods
+#  include Paramable
+
+#  @@artists = []
+
+#  def initialize
+#    super
+#    @songs = []
+#  end
+
+#  def self.all
+#    @@artists
+#  end
+
+  #def self.reset_all
+  #  self.all.clear
+#  end
+
+#  def self.count
+  #  self.all.count
+#  end
+
+#  def add_song(song)
+#    @songs << song
+#    song.artist = self
+#  end
+
+#  def add_songs(songs)
+#    songs.each do { |song| add_song(song) }
+#  end
+#
+#end
 
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -32,11 +63,7 @@ class Artist
     song.artist = self
   end
 
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
+  def add_songs(song)
+    songs.each {|song| add_song(song)}
   end
 end
