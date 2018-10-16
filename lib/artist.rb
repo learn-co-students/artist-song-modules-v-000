945f73eae8a1592c23f880517640ce4418e92927
Achsah Abraham <achsahmp@gmail.com>
2018-10-16 22:42:49 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,53 +1,12 @@
-require 'pry'
-require_relative './memorable.rb'
-require_relative './paramable.rb'
-#class Artist
-#  attr_accessor :name
-#  attr_reader :songs
-#  extend Memorable::ClassMethods
-#  extend Findable
-#  include Memorable::InstanceMethods
-#  include Paramable
-
-#  @@artists = []
-
-#  def initialize
-#    super
-#    @songs = []
-#  end
-
-#  def self.all
-#    @@artists
-#  end
-
-  #def self.reset_all
-  #  self.all.clear
-#  end
-
-#  def self.count
-  #  self.all.count
-#  end
-
-#  def add_song(song)
-#    @songs << song
-#    song.artist = self
-#  end
-
-#  def add_songs(songs)
-#    songs.each do { |song| add_song(song) }
-#  end
-#
-#end
-
 class Artist
   extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
   extend Findable
   include Paramable
-  attr_accessor :name
+  include Memorable::InstanceMethods
+  attr_accessor :name, :artist
   attr_reader :songs
 
-  @@artists = []
+  @@all = []
 
   def initialize
     super
@@ -55,7 +14,11 @@ class Artist
   end
 
   def self.all
-    @@artists
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|a| a.name == name}
   end
 
   def add_song(song)
@@ -63,7 +26,8 @@ class Artist
     song.artist = self
   end
 
-  def add_songs(song)
-    songs.each {|song| add_song(song)}
+  def add_songs(songs)
+    songs.each { |songs| add_song(songs) }
   end
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -6,14 +6,14 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
+  @@all = []
 
   def artist=(artist)
-    @artist - artist
+    @artist = artist
   end
-  
+
   def self.all
-    @@songs
+    @@all
   end
 
 end
