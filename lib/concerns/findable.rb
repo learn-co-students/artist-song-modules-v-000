7035fe9e3c2e5fb6543d68d6d06bc88e3703a1db
Susan Meyers <smmeyers24@gmail.com>
2018-10-18 01:02:17 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,7 @@
 module Findable
-  module ClassMethod
+  module ClassMethods
     def find_by_name(name)
-      artists.all.detect{|a| a.name == name}
-    end
-  end
-    
-    module ClassMethod
-    def find_by_name(name)
-      songs.all.detect{|a| a.name == name}
+      all.detect{|a| a.name == name}
     end
   end
 end
