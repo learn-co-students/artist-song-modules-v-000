4569cae49007d8e3f2c4d2d45c9b401018f38c9e
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-22 20:45:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,15 +19,7 @@ class Song
   def self.all
     @@songs
   end
-
-  #def self.reset_all
-    #self.all.clear
-  #end
-
-  #def self.count
-    #self.all.count
-  #end
-
+  
   def artist=(artist)
     @artist = artist
   end
