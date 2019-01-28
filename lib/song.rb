95fa8cc6fe7def36ecada9a6ee55a6e8ec5ab743
Jamal Gelin <gelin.jamal@gmail.com>
2019-01-28 17:15:43 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
