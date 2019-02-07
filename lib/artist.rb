e8a7d72aaf7533d06ab0f60f17f27d753f711c01
J Fern <jfern20@gmail.com>
2019-02-07 07:27:38 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,24 +10,20 @@ class Artist
     @@artists << self
     @songs = []
   end
+  
+  extend Findable 
+  
+  #def self.find_by_name(name)
+    #@@artists.detect{|a| a.name == name}
+  #end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@artists
-  end
+  #def self.all
+    #@@artists
+  #end
 
  extend Memorable 
 
-  #def self.reset_all
-    #self.all.clear
-  #end
-
-  #def self.count
-    #self.all.count
-  #end
+  
 
   def add_song(song)
     @songs << song
--- lib/song.rb
+++ lib/song.rb
@@ -9,24 +9,20 @@ class Song
   def initialize
     @@songs << self
   end
+   
+   extend Findable 
+  
+  #def self.find_by_name(name)
+    #@@songs.detect{|a| a.name == name}
+  #end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@songs
-  end
+  #def self.all
+    #@@songs
+  #end
 
   extend Memorable 
 
-  #def self.reset_all
-    #self.all.clear
-  #end
-
-  #def self.count
-    #self.all.count
-  #end
+  
 
   def artist=(artist)
     @artist = artist
