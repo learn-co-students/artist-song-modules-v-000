be2a30167badfe318a3d6aafa4b242db5734d2bf
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-28 06:01:22 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 require 'pry'
+# require_relative '../lib/concerns/memorable'
 
 class Song
   attr_accessor :name
@@ -6,31 +7,13 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
