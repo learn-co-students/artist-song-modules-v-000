360307746e6c8092c2bfd125edb087cd89c87403
Carden Z <ephemere@protonmail.com>
2019-04-07 00:23:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,16 @@
-require 'pry'
-
 class Song
-  attr_accessor :name
-  attr_reader :artist
+  extend Memorable
+  extend Findable
+  extend Paramable
 
-  @@songs = []
+  attr_accessor :name
+  attr_reader   :artist
 
   def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
+    @@all << self
   end
 
   def artist=(artist)
     @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
