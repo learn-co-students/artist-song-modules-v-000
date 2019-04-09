d23d8a9fdf56ed60b8d8120b74be887e59f29d43
Toni-Ty <antonique2479@yahoo.com>
2019-04-09 01:51:34 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,12 +11,9 @@ class Artist
     @songs = []
   end
 
-def self.find_by_name(name)
-  @@artists.detect { |artist| artist.name == name}
-
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
 
   def self.all
     @@artists
